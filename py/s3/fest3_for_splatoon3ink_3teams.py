280d569877ddaef7fd6f3851191a88846414fe82
Shahar <shachar700@gmail.com>
2023-07-17 12:17:44 +0300

    updated code to support empty badge slots

--- py/s3/fest3_for_splatoon3ink_3teams.py
+++ py/s3/fest3_for_splatoon3ink_3teams.py
@@ -3,24 +3,24 @@ import base64
 
 # This program reads a JSON file and writes the contents to a text file with wiki markup.
 
-with open("C:/Users/Admin/Downloads/top 100 zelda/festivals.ranking.AP.JUEA-00006.json", 'r', encoding="utf8") as file_in:
+with open("C:/Users/User/Downloads/festivals.ranking.EU.JUEA-00007.json", 'r', encoding="utf8") as file_in:
     data = json.load(file_in)
 
 #data = data['data']['fest']['teams'][2]['result']['rankingHolders']['edges'] #0,1,2 for alpha,bravo,charlie teams
 
 data_dict = {}
-with open('C:/Users/Admin/OneDrive/מסמכים/ink-scripts/py/s3/badgemap.txt', 'r') as mapping:
+with open('C:/Users/User/Documents/github repositories/ink-scripts/py/s3/badgemap.txt', 'r') as mapping:
     for line in mapping:
         k, v = line.strip().split(':')
         data_dict[k.strip()] = v.strip()
 
 #print(data_dict)
 
-with open('C:/Users/Admin/Downloads/top 100 zelda/apfest_parsed.txt', 'w', encoding="utf8") as file_out:
-    #file_out.write("=== Europe ===")
+#create txt file if there aren't any there
+with open('C:/Users/User/Downloads/fest_output.txt', 'w', encoding="utf8") as file_out:
 
     for team in data['data']['fest']['teams']:
-        file_out.write("=== " + team["teamName"] +" ===\n")
+        file_out.write("==== " + team["teamName"] +" ====\n")
         file_out.write("{| class=\"wikitable sitecolor-s3 mw-collapsible mw-collapsed\n")
         file_out.write("! Rank !! Name !! Power !! Weapon !! Title !! <br>Splashtag\n")
         for player in team['result']['rankingHolders']['edges']:
@@ -41,28 +41,41 @@ with open('C:/Users/Admin/Downloads/top 100 zelda/apfest_parsed.txt', 'w', encod
             banner = banner_bytes.decode('ascii')
             banner = banner[20:]
             badges = player["nameplate"]["badges"]
-            badge1 = ''
-            badge2 = ''
-            badge3 = ''
-            if badges != []:
+            badge1 = ""
+            badge2 = ""
+            badge3 = ""
+            print(badges)
+
+            if badges[0] is not None:
                 badge1 = badges[0]["id"]
                 badge1_bytes = badge1.encode('ascii')
                 badge1_bytes = base64.b64decode(badge1_bytes)
                 badge1 = badge1_bytes.decode('ascii')
                 badge1 = badge1[6:]
-            if len(badges) > 1:
+            else:
+                badge1 = "empty"
+
+            if badges[1] is not None:
                 badge2 = badges[1]["id"]
                 badge2_bytes = badge2.encode('ascii')
                 badge2_bytes = base64.b64decode(badge2_bytes)
                 badge2 = badge2_bytes.decode('ascii')
                 badge2 = badge2[6:]
-            if len(badges) > 2:
+            else:
+                badge2 = "empty"
+
+            if badges[2] is not None:
                 badge3 = badges[2]["id"]
                 badge3_bytes = badge3.encode('ascii')
                 badge3_bytes = base64.b64decode(badge3_bytes)
                 badge3 = badge3_bytes.decode('ascii')
                 badge3 = badge3[6:]
+            else:
+                badge3 = "empty"
+
             badgeslist = [badge1,badge2,badge3]
+            print("badgeslist:")
+            print(badgeslist)
             #for item in badgeslist:
                 #mapping.find(badge1)
 
@@ -72,13 +85,15 @@ with open('C:/Users/Admin/Downloads/top 100 zelda/apfest_parsed.txt', 'w', encod
             file_out.write("| " +  str(rank) + " || " + name + " <small>#" + nameid + "</small> || " + str(power)  +
                         " || [[File:S3 Weapon Main " +weapon + " Flat.png|24px|link=" +weapon +
                         "]] [[" + weapon+ "]] || " + title + " || {{UserSplashtag|" + banner)
-
-            if badge1 in data_dict:
-                file_out.write("|" + data_dict[badge1])
-            if badge2 in data_dict:
-                file_out.write("|" + data_dict[badge2])
-            if badge3 in data_dict:
-                file_out.write("|" + data_dict[badge3])
+            if badgeslist == ['empty', 'empty', 'empty']:
+                print("all are empty")
+            else:
+                if badge1 in data_dict:
+                    file_out.write("|" + data_dict[badge1])
+                if badge2 in data_dict:
+                    file_out.write("|" + data_dict[badge2])
+                if badge3 in data_dict:
+                    file_out.write("|" + data_dict[badge3])
             # if badge1 != '':
             #     file_out.write("|" + badgeslist[0])
             # if badge2 != '':
--- py/s3/fest3_for_splatoon3ink_jp_3teams.py
+++ py/s3/fest3_for_splatoon3ink_jp_3teams.py
@@ -3,30 +3,30 @@ import base64
 
 #Update weapons list from: https://stat.ink/api/v3/weapon
 
-with open("C:/Users/Admin/Downloads/top 100 zelda/festivals.ranking.JP.JUEA-00006.json", 'r', encoding="utf8") as file_in:
+with open("C:/Users/User/Downloads/festivals.ranking.JP.JUEA-00007.json", 'r', encoding="utf8") as file_in:
     data = json.load(file_in)
 
 #data = data['data']['fest']['teams'][2]['result']['rankingHolders']['edges'] #0,1,2 for alpha,bravo,charlie teams
 
 #extract mapping of badges from file to dictionary
 data_dict_badges = {}
-with open('C:/Users/Admin/OneDrive/מסמכים/ink-scripts/py/s3/badgemap.txt', 'r') as mapping:
+with open('C:/Users/User/Documents/github repositories/ink-scripts/py/s3/badgemap.txt', 'r') as mapping:
     for line in mapping:
         k, v = line.strip().split(':')
         data_dict_badges[k.strip()] = v.strip()
 
 #extract weapon names from file to dictionary
-with open('C:/Users/Admin/OneDrive/מסמכים/ink-scripts/py/s3/weapons.json', 'r', encoding="utf8") as weapons_json_file:
+with open('C:/Users/User/Documents/github repositories/ink-scripts/py/s3/weapons.json', 'r', encoding="utf8") as weapons_json_file:
     data_weapons = json.load(weapons_json_file)
 
 data_dict_adj = {}
-with open('C:/Users/Admin/OneDrive/מסמכים/ink-scripts/py/s3/titles-adj.txt', 'r', encoding="utf8") as titles_adj:
+with open('C:/Users/User/Documents/github repositories/ink-scripts/py/s3/titles-adj.txt', 'r', encoding="utf8") as titles_adj:
     for line in titles_adj:
         k, v = line.strip().split(':')
         data_dict_adj[k.strip()] = v.strip()
 
 data_dict_subj = {}
-with open('C:/Users/Admin/OneDrive/מסמכים/ink-scripts/py/s3/titles-sbj.txt', 'r', encoding="utf8") as titles_subj:
+with open('C:/Users/User/Documents/github repositories/ink-scripts/py/s3/titles-sbj.txt', 'r', encoding="utf8") as titles_subj:
     for line in titles_subj:
         k, v = line.strip().split(':')
         data_dict_subj[k.strip()] = v.strip()
@@ -36,7 +36,8 @@ with open('C:/Users/Admin/OneDrive/מסמכים/ink-scripts/py/s3/titles-sbj.txt
 #print(data_dict_titles)
 #print(data_dict)
 
-with open("C:/Users/Admin/Downloads/top 100 zelda/jpfest_parsed.txt", 'w', encoding="utf8") as file_out:
+#create txt file if there aren't any there
+with open("C:/Users/User/Downloads/fest_output.txt", 'w', encoding="utf8") as file_out:
 
     for team in data['data']['fest']['teams']:
         file_out.write("=== " + team["teamName"] +" ===\n")
@@ -65,28 +66,38 @@ with open("C:/Users/Admin/Downloads/top 100 zelda/jpfest_parsed.txt", 'w', encod
             banner = banner_bytes.decode('ascii')
             banner = banner[20:]
             badges = player["nameplate"]["badges"]
-            badge1 = ''
-            badge2 = ''
-            badge3 = ''
-            if badges != []:
+            badge1 = ""
+            badge2 = ""
+            badge3 = ""
+            print(badges)
+
+            if badges[0] is not None:
                 badge1 = badges[0]["id"]
                 badge1_bytes = badge1.encode('ascii')
                 badge1_bytes = base64.b64decode(badge1_bytes)
                 badge1 = badge1_bytes.decode('ascii')
                 badge1 = badge1[6:]
-            if len(badges) > 1:
+            else:
+                badge1 = "empty"
+
+            if badges[1] is not None:
                 badge2 = badges[1]["id"]
                 badge2_bytes = badge2.encode('ascii')
                 badge2_bytes = base64.b64decode(badge2_bytes)
                 badge2 = badge2_bytes.decode('ascii')
                 badge2 = badge2[6:]
-            if len(badges) > 2:
+            else:
+                badge2 = "empty"
+
+            if badges[2] is not None:
                 badge3 = badges[2]["id"]
                 badge3_bytes = badge3.encode('ascii')
                 badge3_bytes = base64.b64decode(badge3_bytes)
                 badge3 = badge3_bytes.decode('ascii')
                 badge3 = badge3[6:]
-            badgeslist = [badge1,badge2,badge3]
+            else:
+                badge3 = "empty"
+            badgeslist = [badge1, badge2, badge3]
             #for item in badgeslist:
                 #mapping.find(badge1)
 
@@ -105,12 +116,15 @@ with open("C:/Users/Admin/Downloads/top 100 zelda/jpfest_parsed.txt", 'w', encod
                            " || [[File:S3 Weapon Main " +weapon + " Flat.png|24px|link=" +weapon +
                            "]] [[" + weapon+ "]] || " + adj_title + " " + subj_title + " || {{UserSplashtag|" + banner)
 
-            if badge1 in data_dict_badges:
-                file_out.write("|" + data_dict_badges[badge1])
-            if badge2 in data_dict_badges:
-                file_out.write("|" + data_dict_badges[badge2])
-            if badge3 in data_dict_badges:
-                file_out.write("|" + data_dict_badges[badge3])
+            if badgeslist == ['empty', 'empty', 'empty']:
+                print("all are empty")
+            else:
+                if badge1 in data_dict_badges:
+                    file_out.write("|" + data_dict_badges[badge1])
+                if badge2 in data_dict_badges:
+                    file_out.write("|" + data_dict_badges[badge2])
+                if badge3 in data_dict_badges:
+                    file_out.write("|" + data_dict_badges[badge3])
             # if badge1 != '':
             #     file_out.write("|" + badgeslist[0])
             # if badge2 != '':
